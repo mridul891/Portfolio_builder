fcaeb2f4d4ca6f7832ea116096e01ac7be0af9c1
Mridul Pandey <pandeym891@gmail.com>
2025-08-22 12:03:09 +0530

    Completed the extraction of data for a static file present in the localhost

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,29 @@
-import express from 'express'
+import express from "express";
+import fs from "fs";
+import pdf from "pdf-parse";
+import path from "path";
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-const app = express()
-const PORT = process.env.PORT || 4000
+app.get("/", (req, res) => {
+    const pathName = path.join(__dirname, "../public/sample.pdf"); 
+  let dataBuffer = fs.readFileSync(pathName);
 
-app.get("/", (req ,res) =>{
-    res.json({
-        message : "Welcome to the Portfolio Builder API powered by Mridul Pandey",
+  pdf(dataBuffer)
+    .then(function (data) {
+      // use data
+
+        return res.status(200).json({
+            message : data.text
+        })
     })
-})
+    .catch(function (error) {
+      // handle exceptions
+      console.log(error);
+    });
+ 
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
